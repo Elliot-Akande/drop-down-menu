eb4dc7b05808c88a5e9de90bac45481f7cf6e50f
Elliot Akande <elbo.lynch@gmail.com>
2023-09-13 11:21:05 +0100

    Add default styles

--- src/DropDown.js
+++ src/DropDown.js
@@ -1,14 +1,31 @@
+import './style.css';
+
 const DropDown = function dropDownList(
   title = 'Drop-down',
   items = [
-    { title: 'Item 1', callback: () => {} },
-    { title: 'Item 2', callback: () => {} },
-    { title: 'Item 3', callback: () => {} },
+    {
+      title: 'Item 1',
+      callback: () => {
+        console.log('test');
+      },
+    },
+    {
+      title: 'Item 2',
+      callback: () => {
+        console.log('test');
+      },
+    },
+    {
+      title: 'Item 3',
+      callback: () => {
+        console.log('test');
+      },
+    },
   ],
 ) {
   const createListItem = (data) => {
     const item = document.createElement('div');
-    item.classList.add('drop-down__item');
+    item.classList.add('dropdown__item');
     item.textContent = data.title;
     item.addEventListener('click', data.callback);
     return item;
@@ -17,17 +34,17 @@ const DropDown = function dropDownList(
   const render = () => {
     // Container
     const container = document.createElement('div');
-    container.classList.add('drop-down');
+    container.classList.add('dropdown');
 
     // Button
     const button = document.createElement('div');
-    button.classList.add('drop-down__button');
+    button.classList.add('dropdown__button');
     button.textContent = title;
     container.appendChild(button);
 
     // List
     const list = document.createElement('div');
-    list.classList.add('drop-down__list', 'hidden');
+    list.classList.add('dropdown__list', 'hidden');
     container.appendChild(list);
     items.forEach((item) => list.appendChild(createListItem(item)));
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import './style.css';
 import DropDown from './DropDown';
 
 const body = document.querySelector('body');
