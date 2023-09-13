1041f51b642619edc75c950fad43632831bb5bf5
Elliot Akande <elbo.lynch@gmail.com>
2023-09-13 11:40:33 +0100

    Add separate index style sheet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
+import './index.css';
 import DropDown from './DropDown';
 
-const body = document.querySelector('body');
-const menu = DropDown();
+const container = document.querySelector('.container');
 
-body.appendChild(menu);
+container.appendChild(
+  DropDown('Sports', [
+    { title: 'Football', callback: () => console.log('Football') },
+    { title: 'Basketball', callback: () => console.log('Basketball') },
+    { title: 'Hockey', callback: () => console.log('Hockey') },
+    { title: 'Swimming', callback: () => console.log('Swimming') },
+  ]),
+);
+
+container.appendChild(
+  DropDown('Food', [
+    { title: 'Pizza', callback: () => console.log('Pizza') },
+    { title: 'Lasagne', callback: () => console.log('Lasagne') },
+  ]),
+);
+
+container.appendChild(
+  DropDown('Bits & Bobs', [{ title: 'Bits' }, { title: 'Bobs' }]),
+);
